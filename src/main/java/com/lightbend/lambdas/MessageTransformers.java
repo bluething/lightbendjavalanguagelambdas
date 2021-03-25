308e136d968782133f8729f527008fecf489761c
Habib Ali Machpud <habib.gunadarma@gmail.com>
2021-03-25 15:13:49 +0700

    Replace lambda with method call

--- src/main/java/com/lightbend/lambdas/MessageTransformers.java
+++ src/main/java/com/lightbend/lambdas/MessageTransformers.java
@@ -15,7 +15,7 @@ class MessageTransformers {
     static <T> List<T> mapMessages(List<Message> messages, Function<Message, T> transformer) {
         return messages
                 .stream()
-                .map(message -> transformer.apply(message))
+                .map(transformer::apply)
                 .collect(Collectors.toList());
     }
 
